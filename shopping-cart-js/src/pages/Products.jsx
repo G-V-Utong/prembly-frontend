bb16e992aaf516302172c5a294d9fce54353912e
Godswill <godswillutong@gmail.com>
2025-10-15 13:51:40 +0100

    add products page

---
+++ shopping-cart-js/src/pages/Products.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import data from '../data/products.json'
+// import ProductCard from '../components/ProductCard.jsx'
+import { useDispatch } from 'react-redux'
+import { addToCart } from '../redux/cartSlice.js'
+
+export default function Products() {
+  const dispatch = useDispatch()
+
+  return (
+    <div className="container">
+      <h2>Products</h2>
+      <div className="grid">
+        {data.map((p) => (
+          <ProductCard key={p.id} product={p} onAdd={(prod) => dispatch(addToCart(prod))} />
+        ))}
+      </div>
+    </div>
+  )
+}
