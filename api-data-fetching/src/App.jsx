4ecea5162cdd09600313fd22d4eec56b8c87ad8c
Godswill <godswillutong@gmail.com>
2025-10-15 17:04:50 +0100

    boilerplate code for api-data-fetching challenge

---
+++ api-data-fetching/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+
+export default function App() {
+  const count = useSelector(selectCartCount)
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="brand">API Showcase</div>
+        <div className="nav-links">
+          <Link to="/quotes">Quotes</Link>
+          <Link to="/covid">COVID Stats</Link>
+          <Link to="/users">Random Users</Link>
+        </div>
+      </nav>
+      <main className="container">
+        <Routes>
+          {/* <Route path="/" element={<Quotes />} />
+          <Route path="/quotes" element={<Quotes />} />
+          <Route path="/covid" element={<Covid />} />
+          <Route path="/users" element={<RandomUsers />} /> */}
+        </Routes>
+      </main>
+      <footer className="footer">© {new Date().getFullYear()} API Data Fetching</footer>
+    </div>
+  )
+}
